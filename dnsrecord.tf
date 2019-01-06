9777badede45776c1ea95b0c77cba760744cff94
harshvardhanS <asawat.harsh@gmail.com>
2019-01-06 22:00:08 +0530

    added dns record for devops4you.tk

---
+++ dnsrecord.tf
@@ -0,0 +1,15 @@
+data "aws_route53_zone" "terraform-blue-green" {
+  name = "devops4you.tk."
+}
+
+resource "aws_route53_record" "terraform-blue-green" {
+  zone_id = "${data.aws_route53_zone.terraform-blue-green.zone_id}"
+  name    = "www.devops4you.tk"
+  type    = "A"
+
+  alias {
+    name                   = "dualstack.${aws_elb.terraform-blue-green.dns_name}"
+    zone_id                = "${aws_elb.terraform-blue-green.zone_id}"
+    evaluate_target_health = false
+  }
+}
